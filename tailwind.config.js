42f8fb27df72e9b15de1408864703b81f4834b13
vladfreishmidt <vladfreishmidt.webdevelopment@gmail.com>
2023-02-02 21:32:25 +0200

    Configure Open Sans to work with TailwindCSS

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require("tailwindcss/defaultTheme");
+
 module.exports = {
   content: ["./index.html", "./src/**/*.{vue,js,ts}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ["Open Sans", ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
-}
+};
