b722854ea5c7c950f0f06cb108bf3cbb66848b6f
vladfreishmidt <vladfreishmidt.webdevelopment@gmail.com>
2023-02-03 13:24:53 +0200

    Configure eslint for test globals

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -7,8 +7,12 @@ module.exports = {
     "plugin:vue/vue3-recommended",
     "eslint:recommended",
     "@vue/eslint-config-prettier",
+    "plugin:vites-globals/recommended",
   ],
   parserOptions: {
     ecmaVersion: "latest",
   },
+  env: {
+    'vitest-globals/env': true
+  }
 };
