11b29fd6e795fdbb3024a2dad19f8d12301b8c73
vladfreishmidt <vladfreishmidt.webdevelopment@gmail.com>
2023-02-04 15:44:05 +0200

    Enhance ActionButton component

--- src/components/ActionButton.vue
+++ src/components/ActionButton.vue
@@ -1,13 +1,36 @@
 <template>
-  <button
-    class="rounded border-0 bg-brand-blue-1 px-5 py-3 font-medium text-white hover:shadow-blue"
-  >
-    Sign in
+  <button :class="buttonClass">
+    {{ text }}
   </button>
 </template>
 
 <script>
 export default {
   name: "ActionButton",
+  props: ["text"],
+  data() {
+    return {
+      primary: true,
+    };
+  },
+  computed: {
+    buttonClass() {
+      return { primary: this.primary };
+    },
+  },
 };
 </script>
+
+<style scoped>
+button {
+  @apply rounded px-5 py-3 font-medium;
+}
+
+.primary {
+  @apply border-0 bg-brand-blue-1 text-white hover:shadow-blue;
+}
+
+.secondary {
+  @apply bg-white text-brand-blue-1;
+}
+</style>
--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -24,7 +24,7 @@
 
         <div class="ml-auto flex h-full items-center">
           <profile-image v-if="isLoggedIn" />
-          <action-button v-else @click="loginUser" />
+          <action-button v-else text="Sign in" @click="loginUser" />
         </div>
       </div>
     </div>
