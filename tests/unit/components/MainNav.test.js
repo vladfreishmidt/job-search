7229c421bf71952733030275ead6771207664f8a
vladfreishmidt <vladfreishmidt.webdevelopment@gmail.com>
2023-02-04 11:06:06 +0200

    Add MainNav test for navigation links

--- tests/unit/components/MainNav.test.js
+++ tests/unit/components/MainNav.test.js
@@ -5,7 +5,23 @@ import MainNav from "@/components/MainNav.vue";
 describe("MainNav", () => {
   it("displays company name", () => {
     render(MainNav);
-    const companyName = screen.getByText("Super Corp");
+    const companyName = screen.getByText("Job Careers");
     expect(companyName).toBeInTheDocument();
+  });
+
+  it("displays menu items for navigation", () => {
+    render(MainNav);
+    const navigationMenuItems = screen.getAllByRole("listitem");
+    const navigationMenuTexts = navigationMenuItems.map(
+      (item) => item.textContent
+    );
+    expect(navigationMenuTexts).toEqual([
+      "Teams",
+      "Locations",
+      "Life at Job Search",
+      "How we hire",
+      "Students",
+      "Jobs",
+    ]);
   });
 });
