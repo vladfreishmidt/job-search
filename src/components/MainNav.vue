a5bada9b354b635de7c32877691b4d16b888fdee
vladfreishmidt <vladfreishmidt.webdevelopment@gmail.com>
2023-02-04 12:28:24 +0200

    Add profile image component

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -21,14 +21,26 @@
             </li>
           </ul>
         </nav>
+
+        <div class="ml-auto flex h-full items-center">
+          <profile-image v-if="isLoggedIn" />
+          <action-button v-else @click="loginUser" />
+        </div>
       </div>
     </div>
   </header>
 </template>
 
 <script>
+import ActionButton from "@/components/ActionButton.vue";
+import ProfileImage from "@/components/ProfileImage.vue";
+
 export default {
   name: "MainNav",
+  components: {
+    ActionButton,
+    ProfileImage,
+  },
   data() {
     return {
       company: "Job Careers",
@@ -41,7 +53,13 @@ export default {
         "Students",
         "Jobs",
       ],
+      isLoggedIn: false,
     };
+  },
+  methods: {
+    loginUser() {
+      this.isLoggedIn = true;
+    },
   },
 };
 </script>
---
+++ src/components/ProfileImage.vue
@@ -0,0 +1,19 @@
+<template>
+  <img
+    :src="imageLink"
+    class="h-8 w-8 rounded-3xl object-cover"
+    alt="profile image"
+  />
+</template>
+
+<script>
+export default {
+  name: "ProfileImage",
+  data() {
+    return {
+      imageLink:
+        "https://media.istockphoto.com/id/1338134336/photo/headshot-portrait-african-30s-man-smile-look-at-camera.jpg?b=1&s=170667a&w=0&k=20&c=j-oMdWCMLx5rIx-_W33o3q3aW9CiAWEvv9XrJQ3fTMU=",
+    };
+  },
+};
+</script>
