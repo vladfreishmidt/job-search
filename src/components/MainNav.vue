e474bf2cad670712bcc28ad255fb0d5f74511677
vladfreishmidt <vladfreishmidt.webdevelopment@gmail.com>
2023-02-04 21:04:04 +0200

    Add fontawesome pack

--- src/components/MainNav.vue
+++ src/components/MainNav.vue
@@ -32,7 +32,7 @@
           />
         </div>
       </div>
-      <the-subnav />
+      <the-subnav v-if="isLoggedIn" />
     </div>
   </header>
 </template>
--- src/components/TheSubnav.vue
+++ src/components/TheSubnav.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="sorder-solid h-16 w-full border-b border-brand-gray-1 bg-white">
-    <div class="flex h-full items-center px-8">This is a subnav</div>
+    <div class="flex h-full items-center px-8">
+      <div>
+        <font-awesome-icon :icon="['fas', 'search']" class="mr-3" />
+        <span>
+          <span class="text-brand-green-1">123</span>
+          jobs matched
+        </span>
+      </div>
+    </div>
   </div>
 </template>
 
