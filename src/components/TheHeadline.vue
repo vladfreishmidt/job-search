16e1b52def2499fd7a25763d1f9cc86ca7d3791b
vladfreishmidt <vladfreishmidt.webdevelopment@gmail.com>
2023-02-05 19:17:45 +0200

    Add Headline component

---
+++ src/components/TheHeadline.vue
@@ -0,0 +1,37 @@
+<template>
+  <section>
+    <h1>{{ action }} for everyone</h1>
+    <h2>Find your next job at 'Job Search'</h2>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "TheHeadline",
+  data() {
+    return {
+      action: "Build",
+      interval: null,
+    };
+  },
+  created() {
+    this.changeTitle();
+  },
+  beforeUnmount() {
+    clearInterval(this.interval);
+  },
+  methods: {
+    changeTitle() {
+      this.interval = setInterval(() => {
+        const actions = ["Build", "Create", "Design", "Code"];
+        const currecntActionIndex = actions.indexOf(this.action);
+        const nextActionIndex = (currecntActionIndex + 1) % actions.length;
+        const nextAction = actions[nextActionIndex];
+        this.action = nextAction;
+      }, 3000);
+    },
+  },
+};
+</script>
+
+<style scoped></style>
