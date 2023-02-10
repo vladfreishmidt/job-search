ada8dcb814862330660ea868baef049cf5e40e47
vladfreishmidt <vladfreishmidt.webdevelopment@gmail.com>
2023-02-10 12:15:47 +0200

    Add route to a job page

---
+++ src/views/JobView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Job page for job {{ currentJobId }}</div>
+</template>
+
+<script>
+export default {
+  name: "JobView",
+  computed: {
+    currentJobId() {
+      return this.$route.params.id;
+    },
+  },
+};
+</script>
